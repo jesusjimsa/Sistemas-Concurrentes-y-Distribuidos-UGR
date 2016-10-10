61b093d610b6019d2da320a1fefeffa67fcec1bd
jesusjimsa <jesusjimsa@icloud.com>
2016-10-10 20:32:25 +0200

    Cambios en la estructura del código

--- Práctica 1/fumadores.cpp
+++ Práctica 1/fumadores.cpp
@@ -1,10 +1,3 @@
-// *****************************************************************************
-//
-// Prácticas de SCD. Práctica 1.
-// Plantilla de código para el ejercicio de los fumadores
-//
-// *****************************************************************************
-
 #include <iostream>
 #include <cassert>
 #include <pthread.h>
@@ -19,17 +12,21 @@ using namespace std ;
 // introduce un retraso aleatorio de duración comprendida entre
 // 'smin' y 'smax' (dados en segundos)
 
-void retraso_aleatorio( const float smin, const float smax )
-{
-  static bool primera = true ;
-  if ( primera )        // si es la primera vez:
-  {  srand(time(NULL)); //   inicializar la semilla del generador
-     primera = false ;  //   no repetir la inicialización
-  }
-  // calcular un número de segundos aleatorio, entre {\ttbf smin} y {\ttbf smax}
-  const float tsec = smin+(smax-smin)*((float)random()/(float)RAND_MAX);
-  // dormir la hebra (los segundos se pasan a microsegundos, multiplicándos por 1 millón)
-  usleep( (useconds_t) (tsec*1000000.0)  );
+void retraso_aleatorio( const float smin, const float smax ){
+	static bool primera = true ;
+	
+	if ( primera ){        // si es la primera vez:
+		srand(time(NULL)); //   inicializar la semilla del generador
+		primera = false ;  //   no repetir la inicialización
+	}
+	
+	// calcular un número de segundos aleatorio, entre {\ttbf smin} y {\ttbf smax}
+	
+	const float tsec = smin+(smax-smin)*((float)random()/(float)RAND_MAX);
+	
+	// dormir la hebra (los segundos se pasan a microsegundos, multiplicándos por 1 millón)
+	
+	usleep( (useconds_t) (tsec*1000000.0)  );
 }
 
 // ----------------------------------------------------------------------------
@@ -37,12 +34,14 @@ void retraso_aleatorio( const float smin, const float smax )
 // recibe como parámetro el numero de fumador
 // el tiempo que tarda en fumar está entre dos y ocho décimas de segundo.
 
-void fumar( int num_fumador )
-{
-   cout << "Fumador número " << num_fumador << ": comienza a fumar." << endl << flush ;
-   retraso_aleatorio( 0.2, 0.8 );
-   cout << "Fumador número " << num_fumador << ": termina de fumar." << endl << flush ;
+void fumar( int num_fumador ){
+	cout << "Fumador número " << num_fumador << ": comienza a fumar." << endl << flush ;
+	
+	retraso_aleatorio( 0.2, 0.8 );
+	
+	cout << "Fumador número " << num_fumador << ": termina de fumar." << endl << flush ;
 }
+
 // ----------------------------------------------------------------------------
 
 // falta: resto de funciones
@@ -50,10 +49,9 @@ void fumar( int num_fumador )
 
 // ----------------------------------------------------------------------------
 
-int main()
-{
-  srand( time(NULL) ); // inicializa semilla aleatoria para selección aleatoria de fumador
-  // falta: creación hebras ....
-
-  return 0 ;
+int main(){
+	srand( time(NULL) ); // inicializa semilla aleatoria para selección aleatoria de fumador
+	// falta: creación hebras ....
+	
+	return 0 ;
 }
