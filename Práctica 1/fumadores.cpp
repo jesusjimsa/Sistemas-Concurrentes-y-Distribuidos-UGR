07ba4fb720cf0340e29daca61d411871a64421e6
jesusjimsa <jesusjimsa@icloud.com>
2016-10-10 21:30:01 +0200

    Implementación estanquero y fumador

--- Práctica 1/fumadores.cpp
+++ Práctica 1/fumadores.cpp
@@ -8,6 +8,10 @@
 
 using namespace std ;
 
+sem_t estanquero, fumador[3];	//Declaracion de los semáforos
+
+enum class Ingredientes{tabaco, papel, cerillas};	//Declaración de los ingredientes con un enumerado
+
 // ---------------------------------------------------------------------
 // introduce un retraso aleatorio de duración comprendida entre
 // 'smin' y 'smax' (dados en segundos)
@@ -35,23 +39,60 @@ void retraso_aleatorio( const float smin, const float smax ){
 // el tiempo que tarda en fumar está entre dos y ocho décimas de segundo.
 
 void fumar( int num_fumador ){
-	cout << "Fumador número " << num_fumador << ": comienza a fumar." << endl << flush ;
 	
-	retraso_aleatorio( 0.2, 0.8 );
-	
-	cout << "Fumador número " << num_fumador << ": termina de fumar." << endl << flush ;
+	while(true){
+		sem_wait(&fumador[num_fumador]);
+		
+		cout << "Fumador número " << num_fumador << ": comienza a fumar." << endl << flush ;
+		
+		retraso_aleatorio( 0.2, 0.8 );
+		
+		cout << "Fumador número " << num_fumador << ": termina de fumar." << endl << flush ;
+		
+		sem_post(&estanquero);
+	}
 }
 
 // ----------------------------------------------------------------------------
+//Función que simula al estanquero
+//Crea aleatoriamente los ingredientes
 
-// falta: resto de funciones
-// ..............
-
-// ----------------------------------------------------------------------------
+void estanquero(){
+	srand(time(NULL));
+	Ingredientes creado;
+	
+	while (true){
+		sem_wait(estanquero);
+		
+		creado = Ingredientes::rand()%3;
+		
+		if (creado == Ingredientes::cerillas){
+			sem_post(&fumador[1]);
+		}
+		
+		if (creado == Ingredientes::tabaco){
+			sem_post(&fumador[2]);
+		}
+		
+		if(creado == Ingredientes::papel){
+			sem_post(&fumador[3]);
+		}
+	}
+}
 
 int main(){
 	srand( time(NULL) ); // inicializa semilla aleatoria para selección aleatoria de fumador
-	// falta: creación hebras ....
+	pthread_t fumador_1, fumador_2, fumador_3, estanquero;	//Declaración de las diferentes hebras
+	
+	//Inicialización de los semáforos
+	
+	for (int i = 0; i < 3; i++){
+		sem_init(&fumador[i], 0, 0);
+	}
+	sem_init(estanquero, 0, 1);
+	
+	//Creación de hebras
+	
 	
 	return 0 ;
 }
