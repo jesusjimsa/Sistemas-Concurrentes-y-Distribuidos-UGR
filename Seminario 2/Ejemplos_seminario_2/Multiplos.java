48ecd2226463abe27749cc08598d05f622a1dc7b
jesusjimsa <jesusjimsa@icloud.com>
2016-11-04 10:32:59 +0100

    Multiplos

--- Seminario 2/Ejemplos_seminario_2/Multiplos.java
+++ Seminario 2/Ejemplos_seminario_2/Multiplos.java
@@ -28,7 +28,7 @@ class Contador
   // -------------------------------------------------------------------
   // incrementa valor en 1 
   
-  public synchronized void incrementa () 
+  public synchronized void incrementa ()
   { long aux = valor ; // hace copia local del valor actual
     retrasoOcupado() ; // permite entrelazado cuando no se hace en EM
     valor = aux+1 ;    // escribe el valor compartido (incrementado)
